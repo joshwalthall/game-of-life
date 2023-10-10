f415ffb624cb8bf01ae2f475704e29f5df6c8c76
Josh Walthall <josh.walthall@gmail.com>
2023-10-09 20:38:53 -0400

    Implement GameFactory and Tick button

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-const aliveColor = '#FFFFFF';
-const deadColor = '#000000';
-const hoverColor = '#808080'
 const neighborTranslations = [[-1,-1], [0,-1], [1,-1], [-1,0], [1,0], [-1,1], [0,1], [1,1]];
 
-let gridCount = 64; // Number of x and y grid squares
+let gridCount = 32; // Number of x and y grid squares
 let gridContainerSize = 800; // Will be converted to size in pixels
 let gridTileSize = `${gridContainerSize / gridCount}px`;
 let minSurvival = 2;
@@ -12,6 +9,52 @@ let minBirth = 3;
 let maxBirth = 3;
 
 const gridContainer = document.querySelector('#grid-container');
+const tickButton = document.querySelector('#tick-button');
+
+const GameFactory = () => {
+    const states = ["paused", "running"];
+    let state = states[0];
+    let cells = [];
+
+    const createGameGrid = () => {
+        // Set grid container rows and columns amounts and sizes
+        gridContainer.style.gridTemplateRows = `repeat(${gridCount}, ${gridTileSize})`;
+        gridContainer.style.gridTemplateColumns = `repeat(${gridCount}, ${gridTileSize})`;
+        // Iterate through each row
+        for (y = 1; y <= gridCount; y++) {
+            for (x = 1; x <= gridCount; x++) {
+                let cellTile = document.createElement('div');
+                let newCell = CellFactory(cellTile, x,y);
+                cells.push(newCell);
+                let cellPosition = newCell.getPosition();
+                cellTile.style.gridArea = `${y} / ${x} / ${y+1} / ${x+1}`;
+                cellTile.style.width = gridTileSize;
+                cellTile.style.height = gridTileSize;
+                cellTile.classList.add('cell-tile');
+                cellTile.classList.add('dead');
+                cellTile.dataset.positionX = `${cellPosition[0]}`;
+                cellTile.dataset.positionY = `${cellPosition[1]}`;
+                cellTile.addEventListener('mouseenter', newCell.mouseEnter);
+                cellTile.addEventListener('mouseleave', newCell.mouseLeave);
+                cellTile.addEventListener('mousedown', newCell.swapState);
+                cellTile.addEventListener('mousedown', newCell.countLiveNeighbors);
+                gridContainer.appendChild(cellTile);
+            };
+        };
+    };
+    const getNextCellState = (cell) => {
+        cell.getNextAliveState();
+    };
+    const setNextCellState = (cell) => {
+        cell.setNextAliveState();
+    };
+    const tick = () => {
+        cells.forEach(getNextCellState);
+        cells.forEach(setNextCellState);
+    };
+
+    return {cells, createGameGrid, tick};
+};
 
 const CellFactory = (cellTile, xPos, yPos) => {
     const tile = cellTile;
@@ -19,88 +62,83 @@ const CellFactory = (cellTile, xPos, yPos) => {
     const positionY = yPos;
     const position = [positionX, positionY];
     let isAlive = false;
-    let liveNeighborCount = 0;
+    let nextAliveState = false;
 
     const _live = () => {
         isAlive = true;
+        tile.classList.remove('dead');
+        tile.classList.add('alive');
     };
     const _die = () => {
         isAlive = false;
+        tile.classList.remove('alive');
+        tile.classList.add('dead');
     };
-    const _setTileColor = () => {
-        if (isAlive) {
-            tile.style.backgroundColor = aliveColor;
-        } else if (!isAlive) {
-            tile.style.backgroundColor = deadColor;
+    const _countLiveNeighbors = () => {
+        let liveNeighbors = 0;
+        for (i = 0; i < 8; i++) {
+            let neighborTranslate = neighborTranslations[i];
+            let neighborPosX = (positionX + neighborTranslate[0]);
+            if (neighborPosX === 0) {
+                neighborPosX = gridCount;
+            } else if (neighborPosX === (gridCount + 1)) {
+                neighborPosX = 1;
+            };
+            let neighborPosY = (positionY + neighborTranslate[1]);
+            if (neighborPosY === 0) {
+                neighborPosY = gridCount;
+            } else if (neighborPosY === (gridCount + 1)) {
+                neighborPosY = 1;
+            };
+            let neighborTile = document.querySelectorAll(
+                `[data-position-x='${neighborPosX}'][data-position-y='${neighborPosY}']`
+            )[0];
+            if (neighborTile.classList.contains("alive")) {
+                liveNeighbors += 1;
+            };
         };
-    };
-
-    const mouseEnter = () => {
-        tile.style.backgroundColor = hoverColor;
-    };
-    const mouseLeave = () => {
-        _setTileColor();
+        return liveNeighbors;
     };
     const getPosition = () => {
         return position;
     };
-    const countLiveNeighbors = () => {
-        liveNeighborCount = 0;
-        for (i = 0; i < 8; i++) {
-            let neighborTranslate = neighborTranslations[i];
-            let neighborPosX = (positionX + neighborTranslate[0]);
-            let neighborPosY = (positionY + neighborTranslate[1]);
-            let neighborTile = document.querySelectorAll(
-                `[data-position-x='${neighborPosX}'][data-position-y='${neighborPosY}']`
-            )[0];
-            if (neighborTile.dataset.isAlive === "true") {
-                liveNeighborCount += 1;
-            };
+    const getAliveState = () => {
+        return isAlive;
+    };
+    const getNextAliveState = () => {
+        _countLiveNeighbors();
+        let liveNeighbors = _countLiveNeighbors();
+        if (isAlive === true && liveNeighbors >= minSurvival && liveNeighbors <= maxSurvival) {
+            nextAliveState = true;
+        } else if (isAlive === false && liveNeighbors >= minBirth && liveNeighbors <= maxBirth) {
+            nextAliveState = true;
+        } else {
+            nextAliveState = false;
         };
-        console.log(`Live Neighbors: ${liveNeighborCount}`);
+    };
+    const setNextAliveState = () => {
+        if (isAlive === false && nextAliveState === true) {
+            _live();
+        } else if (isAlive === true && nextAliveState === false) {
+            _die();
+        };
     };
     const swapState = () => {
         if (isAlive === true) {
             _die();
-            tile.dataset.isAlive = isAlive;
         } else if (isAlive === false)  {
             _live();
-            tile.dataset.isAlive = isAlive;
         };
-        _setTileColor();
     };
-
-    return {isAlive, getPosition, mouseEnter, mouseLeave, countLiveNeighbors, swapState};
-};
-
-function createGameGrid() {
-    // Set grid container rows and columns amounts and sizes
-    gridContainer.style.gridTemplateRows = `repeat(${gridCount}, ${gridTileSize})`;
-    gridContainer.style.gridTemplateColumns = `repeat(${gridCount}, ${gridTileSize})`;
-    // Iterate through each row
-    for (y = 1; y <= gridCount; y++) {
-        for (x = 1; x <= gridCount; x++) {
-            let cellTile = document.createElement('div');
-            let newCell = CellFactory(cellTile, x,y);
-            console.log('Created new cell');
-            let cellPosition = newCell.getPosition();
-            console.log(`New Cell Position: ${cellPosition}`);
-            cellTile.style.gridArea = `${y} / ${x} / ${y+1} / ${x+1}`;
-            cellTile.style.width = gridTileSize;
-            cellTile.style.height = gridTileSize;
-            cellTile.classList.add('cell-tile');
-            cellTile.dataset.isAlive = 'false';
-            cellTile.style.backgroundColor = deadColor;
-            cellTile.dataset.isAlive = `${newCell.isAlive}`;
-            cellTile.dataset.positionX = `${cellPosition[0]}`;
-            cellTile.dataset.positionY = `${cellPosition[1]}`;
-            cellTile.addEventListener('mouseenter', newCell.mouseEnter);
-            cellTile.addEventListener('mouseleave', newCell.mouseLeave);
-            cellTile.addEventListener('mousedown', newCell.swapState);
-            cellTile.addEventListener('mousedown', newCell.countLiveNeighbors);
-            gridContainer.appendChild(cellTile);
-        };
+    return {
+        getPosition,
+        getAliveState,
+        getNextAliveState,
+        setNextAliveState,
+        swapState
     };
 };
 
-createGameGrid();
+const game = GameFactory();
+game.createGameGrid();
+tickButton.addEventListener('click', game.tick);
