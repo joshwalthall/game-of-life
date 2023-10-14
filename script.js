4de1d196a8e6df4f872952657d6494379591f461
Josh Walthall <josh.walthall@gmail.com>
2023-10-14 14:18:23 -0400

    Change style and color on all elements; Add Dosis font

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ const neighborTranslations = [
 ];
 
 let gridCount = 128; // Number of x and y grid squares
-let gridContainerSize = 800; // Will be converted to size in pixels
-let gridTileSize = `${gridContainerSize / gridCount}px`;
-let lifeChance = 0.2;
-let minSurvival = 2;
-let maxSurvival = 3;
-let minBirth = 3;
-let maxBirth = 3;
-let tickRate = 250; // Time in ms between each tick
+let gridContainerSize = 700; // Will be converted to size in pixels
+let gridTileSize = `${gridContainerSize / gridCount}px`; // Size of each cell in pixels
+let lifeChance = 0.3; // Odds of randomly generated cell being alive
+let minSurvival = 2; // Minimum alive neighbors for cell to stay alive
+let maxSurvival = 3; // Maximum alive neighbors for cell to stay alive
+let minBirth = 3; // Minimum alive neighbors for dead cell to come to life
+let maxBirth = 3; // Maximum alive neighbors for dead cell to come to life
+let tickRate = 200; // Time in ms between each tick
 
 const gridContainer = document.querySelector('#grid-container');
 const tickButton = document.querySelector('#tick-button');
